13b9712816c01135e330450f55c2de9cea47de7b
Wajahat Ullah Khan <wukhan547@gmail.com>
2024-11-05 09:30:27 +0500

    Update main

--- cl.py
+++ cl.py
@@ -2,7 +2,7 @@ import random
 from pymongo import MongoClient
 
 # Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("your mongo db key")
 db = client['bank']
 collection1 = db['currentaccount']
 collection2 = db['savingaccount']
